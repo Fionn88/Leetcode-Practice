db5db7956fb92c8c0b155b97b40006e450825b7c
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2023-09-19 22:36:20 +0800

    improve the algorithm
    Signed-off-by: Yuan-Heng,Kuo <coco8802231225@gmail.com>

--- neetcode/arrays_and_hashing/medium/49_Group Anagrams.py
+++ neetcode/arrays_and_hashing/medium/49_Group Anagrams.py
@@ -1,36 +1,28 @@
 """
 Runtime
-93ms
-Beats 85.39%of users with Python3
+89ms
+Beats 93.77%of users with Python3
 
 Memory
-20 MB
-Beats 61.07%of users with Python3
-"""
+19.59MB
+Beats 83.48%of users with Python3
+"""    
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # print(strs) => ["eat","tea","tan","ate","nat","bat"]
         D = dict()
+        result_list = []
+        
         # group the anagrams
         for i in range(len(strs)):
             D.setdefault(''.join(sorted(strs[i])), []).append(strs[i])
 
         # {'aet': ['eat', 'tea', 'ate'], 'ant': ['tan', 'nat'], 'abt': ['bat']}
-
+        
         # Sort the value of the dictionary
-        for key,value in D.items():
-            D[key] = sorted(value)
+        for value in D.values():
+            result_list.append(value)
 
-        # {'aet': ['ate', 'eat', 'tea'], 'ant': ['nat', 'tan'], 'abt': ['bat']}
-        
-        # Sort by length of the value
-        sorted_items = sorted(D.items(), key=lambda x: len(x[1]))
-
-        # [('abt', ['bat']), ('ant', ['nat', 'tan']), ('aet', ['ate', 'eat', 'tea'])]
-
-        # Get the value of the sorted_items
-        output = [item[1] for item in sorted_items]
-        
-        # [['bat'], ['nat', 'tan'], ['ate', 'eat', 'tea']]
-        return output
+        # [["eat","tea","ate"],["tan","nat"],["bat"]]
+        return result_list
