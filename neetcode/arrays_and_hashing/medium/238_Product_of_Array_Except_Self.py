32856d14427f790f428be5664cc7cd66b263c78b
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2023-09-21 02:09:36 +0800

    add Product_of_Array_Except_Self solution

---
+++ neetcode/arrays_and_hashing/medium/238_Product_of_Array_Except_Self.py
@@ -0,0 +1,41 @@
+"""
+Runtime
+234ms
+Beats 12.48%of users with Python3
+
+Memory
+40.69MB
+Beats 5.87%of users with Python3
+"""
+import numpy as np
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product,answer = [],[]
+        all_product = 1
+        flag = False
+        for i in nums:
+            all_product = all_product * i
+            if i == 0:
+                flag = True
+
+        if flag:
+            for index in range(len(nums)):
+                if index == 0:
+                    product.append(nums[1:len(nums)])
+                elif index == len(nums):
+                    product.append(nums[:len(nums)-1])
+                else:
+                    right = nums[index+1:len(nums)]
+                    left = nums[:index]
+                    right.extend(left)
+                    product.append(right)
+
+            for j in product:
+                answer.append(np.prod(j))
+        else:
+            for k in nums:
+                answer.append(all_product // k)
+            
+        return answer
+    
+            
