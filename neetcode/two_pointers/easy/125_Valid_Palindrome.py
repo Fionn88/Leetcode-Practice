c50c560b4a4ef436ae99e2000c8de2020090ec35
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2023-09-27 23:16:30 +0800

    add solution

--- neetcode/two_pointers/easy/125_Valid_Palindrome.py
+++ neetcode/two_pointers/easy/125_Valid_Palindrome.py
@@ -11,3 +11,39 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_string = re.sub('[^A-Za-z0-9]+', '', s).lower()
         return new_string == new_string[::-1]
+
+"""
+Runtime
+39ms
+Beats 95.39%of users with Python3
+
+Memory
+17.5MB
+Beats 35.32%of users with Python3
+"""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_string = ''.join(e for e in s if e.isalnum()).lower()
+        return new_string == new_string[::-1]
+
+"""
+Runtime
+51ms
+Beats 53.06%of users with Python3
+
+Memory
+17.75MB
+Beats 32.10%of users with Python3
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_string = ''.join(e for e in s if e.isalnum()).lower()
+        r = 0
+        l = len(new_string) -1
+        while r < l:
+            if new_string[r] != new_string[l]:
+                return False
+            r += 1
+            l -= 1
+        return True
--- neetcode/two_pointers/medium/Two_Sum_II_Input_Array_Is_Sorted.py
+++ neetcode/two_pointers/medium/Two_Sum_II_Input_Array_Is_Sorted.py
@@ -1,1 +1,20 @@
+"""
+Runtime
+122ms
+Beats 42.21%of users with Python3
 
+Memory
+17.08MB
+Beats 98.01%of users with Python3
+"""
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        r = 0
+        l = len(numbers) - 1
+        while r < l:
+            if numbers[r] + numbers[l] > target:
+                l -= 1
+            elif numbers[r] + numbers[l] < target:
+                r += 1
+            elif numbers[r] + numbers[l] == target:
+                return [r+1,l+1]
