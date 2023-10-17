e6d6082ebc1fc62bbab38e086d85525353dba6d1
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2023-10-17 21:23:46 +0800

    add solution

--- arrays_and_hashing/medium/36_Valid_Sudoku.py
+++ arrays_and_hashing/medium/36_Valid_Sudoku.py
@@ -7,35 +7,34 @@ Memory
 16.14MB
 Beats 95.42% of users with Python3
 """
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+# Time Complexity：O(n^2)
+# Space Complexity：O(1)
+# Approach 1 ：Check for duplicate numbers in the vertical, horizontal, and 3x3 grid.
+def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check for duplicate numbers in the vertical
         for row in board:
             dup = {x for x in row if row.count(x) > 1}
-            try:
+            if '.' in dup:
                 dup.remove('.')
-            except:
-                pass 
             if dup:
                 return False 
 
+        # Check for duplicate numbers in the horizontal
         for col in range(9):
             column = [board[row][col] for row in range(9)]
             dup = {x for x in column if column.count(x) > 1}
-            try:
+            if '.' in dup:
                 dup.remove('.')
-            except:
-                pass 
             if dup:
                 return False 
 
+        # Check for duplicate numbers in the 3x3 grid
         for i in range(0, 9, 3):
             for j in range(0, 9, 3):
                 square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
                 dup = {x for x in square if square.count(x) > 1}
-                try:
+                if '.' in dup:
                     dup.remove('.')
-                except:
-                    pass 
                 if dup:
                     return False 
         return True
--- stack/medium/155_Min_Stack.py
+++ stack/medium/155_Min_Stack.py
@@ -1,18 +1,40 @@
+"""
+Runtime
+58ms
+Beats 75.45%of users with Python3
+
+Memory
+20.34MB
+Beats 56.27%of users with Python3
+"""
+# Time Complexity：O(1)
+# Space Complexity：O(1)
+# Approach 1 ：Create two lists, one to record actions and the other to obtain the current minimum value.
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []
+        self.minStack = []
 
     def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            val = min(self.minStack[-1], val)
+
+        self.minStack.append(val)
         
 
     def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
         
 
     def top(self) -> int:
+        return self.stack[-1]
         
 
     def getMin(self) -> int:
+        return self.minStack[-1]
         
 
 
